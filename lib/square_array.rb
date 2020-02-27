6d30097dead6116306cd9c22600685a880d8a20c
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-27 05:40:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   counter = 0
   while counter < array.length do
-    new_array = array[counter].square 
+    new_array = array[counter] ** 2 
     return new_array
     counter +=1
   end 
