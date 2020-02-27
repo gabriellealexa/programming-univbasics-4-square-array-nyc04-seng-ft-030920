9ebbbfed1ed8e9a735bca438a8332941ece9bf6f
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-27 05:42:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   counter = 0
   while counter < array.length do
-    new_array = array[counter] ** 2 
+    new_array = array[counter]
     return new_array
     counter +=1
   end 
+  new_array = 
 end
